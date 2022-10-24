2cf0ec6f4b04788b60a1d38b20911ee7952fec4c
Govindu Venkataramana <AD2NNZZ@mmm.com>
2022-10-24 11:45:39 +0530

    added Register component content

--- src/app/modules/login/register/register.component.ts
+++ src/app/modules/login/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    name: new FormControl('')
+  });
+
+  submit() {
+    if (this.form.valid) {
+      this.submitEM.emit(this.form.value);
+    }
+  }
+  @Input() error: string | null | undefined;
+
+  @Output() submitEM = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
